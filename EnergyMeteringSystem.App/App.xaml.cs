9800324f385384cb4585f21e43b6d63b34cda635
rimmuhiarov2 <rimmuhiarov2@gmail.com>
2026-05-22 12:23:33 +0500

    Попытка сдлеать бд через пк

--- EnergyMeteringSystem.App/App.xaml.cs
+++ EnergyMeteringSystem.App/App.xaml.cs
@@ -4,16 +4,12 @@ using System.Windows;
 
 namespace EnergyMeteringSystem.App
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            // Регистрация сервиса (можно использовать простой DI)
             var exportDialogService = new ExportDialogService();
             var exportService = new ExportService(exportDialogService);
 
